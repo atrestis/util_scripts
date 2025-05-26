5052406f996262f95984c6581052154af27dd28d
Atrestis Karalis <atrestis.karalis@ansys.com>
2025-05-26 10:38:46 +0300

    Fix gdelete.sh reporting files as cleaned up

--- bash_scripts/gdelete.sh
+++ bash_scripts/gdelete.sh
@@ -4,7 +4,7 @@
 git fetch --prune > /dev/null 2>&1
 
 # Updated to match git version 2.35.2 from modulefiles
-branches_to_delete=$(git branch -vv | grep -E '\[origin/.*: gone\]' | awk '{print $1}')
+branches_to_delete=$(git branch -vv | grep -E '\[origin/.*: gone\]' | awk '{if ($1 == "*") {print $2} else {print $1}}')
 
 # Delete each branch that is both fully merged and in the list to delete
 for branch in $branches_to_delete; do
